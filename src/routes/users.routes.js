9ed3b2ef38048120c8d7a70f45082524cbebdfee
Luis Villamizar <luisca123c@gmail.com>
2026-03-11 13:34:03 -0500

    Se crea el archivo de las rutas de usuarios correctamente

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,6 +1,18 @@
-export const getUsers = (req, res) => {
-  res.json({ message: 'Se listaran los usuarios' })
-}
-export const postUser = (req, res) => {
-    res.json({ message: 'Se creara un nuevo usuario' })
-}
+import { postUser, getUsers, getUserById, updateUser, deleteUser, updateUserStatus } from '../controllers/users.controller.js';
+
+import express from 'express';
+const router = express.Router();
+
+router.post('/users', postUser);
+
+router.get('/users', getUsers);
+
+router.get('/users/:id', getUserById);
+
+router.put('/users/:id', updateUser);
+
+router.delete('/users/:id', deleteUser);
+
+router.patch('/users/:id/status', updateUserStatus);
+
+export default router;
